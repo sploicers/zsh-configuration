91e955a976aff4d630e0fa765521b9981bc17a39
sploicers <sploicers@users.noreply.github.com>
2022-07-20 23:33:40 +1200

    Add aliases.zsh, update .gitignore to not ignore .zsh files in /custom, remove example.zsh

---
+++ custom/aliases.zsh
@@ -0,0 +1,16 @@
+# overrides of built-in oh-my-zsh aliases
+alias gb="git --no-pager branch"
+alias lsa="ls -a"
+
+# other aliases
+alias aliasview="ccat $ZSH_CUSTOM/aliases.zsh"
+alias aliasedit="code $ZSH_CUSTOM/aliases.zsh"
+alias reload="source ~/.zshrc && clear"
+alias repos="cd ~/repos"
+alias gs="git status"
+alias lsg="ls | grep"
+alias cdu="cd ../"
+alias cat="ccat --bg=dark"
+alias catg="cat | grep"
+alias kubetcl="minikube kubectl --"
+
